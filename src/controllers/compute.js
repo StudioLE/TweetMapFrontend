9aa008a0e6bf9dfcb942b17906a251d5a2436f51
Laurence Elsdon <git@elsdon.me>
2016-04-15 15:08:11 +0100

    Adjusted method

--- src/controllers/compute.js
+++ src/controllers/compute.js
@@ -25,7 +25,7 @@ angular.module('app.compute', ['ngRoute'])
    * Get data from local storage
    */
   var criteria = Criteria.get()
-  var places = []
+  var tweets = []
 
   $scope.status = 'Computing'
 
@@ -41,19 +41,13 @@ angular.module('app.compute', ['ngRoute'])
       // when the response is available
       console.log(response)
 
-      Tweet.add({
+      tweets.push({
         term: term.text,
         colour: 'black',
         tweets: response.data
       })
 
       callback(null)
-
-      // $scope.status = 'Computation complete'
-      // $location.path('/schedule')
-      // $scope.$apply()
-      // window.location.href = '/#/schedule'
-
 
     }, function errorCallback(response) {
       // called asynchronously if an error occurs
@@ -63,120 +57,15 @@ angular.module('app.compute', ['ngRoute'])
       callback(response)
     })
   }, function(err) {
-      if(err) console.error(err)
+    if(err) console.error(err)
 
-      // Tweet.set(response.data)
+    Tweet.set(tweets)
 
-      $scope.status = 'Computation complete'
-      $location.path('/schedule')
-      // $scope.$apply()
-      // window.location.href = '/#/schedule'
+    $scope.status = 'Computation complete'
+    $location.path('/schedule')
+    // $scope.$apply()
+    // window.location.href = '/#/schedule'
 
-    })
-
-
-
-
-
-  // var map = new google.maps.Map(document.getElementById('map'), {
-  //   center: criteria.city.geometry.location,
-  //   zoom: 13
-  // })
-
-  // var infowindow = new google.maps.InfoWindow()
-
-  // var service = new google.maps.places.PlacesService(map)
-
-  // var types = _.reduce(criteria.types, function(memo, val, key) {
-  //   if (val) memo.push(key)
-  //   return memo
-  // }, [])
-
-  // async.eachSeries(types, function(type, callback) {
-
-  //   // console.log(type)
-
-  //   $scope.status = 'Locating ' + type
-  //   if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') {
-  //     $scope.$apply()
-  //   }
-
-  //   service.nearbySearch({
-  //     bounds: criteria.city.geometry.viewport,
-  //     type: [type]
-  //   }, function (results, status) {
-  //     // console.log(results)
-  //     if (status === google.maps.places.PlacesServiceStatus.OK) {
-  //       places = places.concat(results)
-  //       Place.set(places)
-  //       callback()
-  //     }
-  //     else {
-  //       // callback(status)
-  //       callback()
-  //     }
-  //   })
-  // }, function(err) {
-  //   if(err) console.error(err)
-
-  //   calculateConnections(places)
-  // })
-
-  // var calculateConnections = function(places) {
-  //   var destinations = places
-  //   // console.log('Analysing connections')
-  //   $scope.status = 'Analysing connections'
-  //   $scope.$apply()
-
-  //   var connections = []
-  //   var analysed = []
-
-  //   async.eachSeries(places, function(place, callback) {
-
-  //     $scope.status = 'Analysing ' + place.name
-  //     $scope.$apply()
-
-  //     // destinations.shift()
-  //     // console.log(place.name)
-  //     // console.log(destinations)
-
-  //     async.eachSeries(destinations, function(destination, callback2) {
-  //       // Skip if place = destination
-  //       if(place.id == destination.id) return callback2()
-  //       // Skip if this destination has already been analysed
-  //       if(_.includes(analysed, destination.id)) return callback2()
-
-  //       var distance = Calc.haversinePlaces(place, destination)
-  //       // console.log(distance)
-  //       if(distance <= criteria.connection.distance) {
-  //         var c = {
-  //           data: {
-  //             id: place.id + '-' + destination.id,
-  //             source: place.id,
-  //             target: destination.id
-  //           }
-  //         }
-  //         connections.push(c)
-  //       }
-  //       callback2()
-  //     }, function(err) {
-  //       if(err) console.error(err)
-  //       analysed.push(place.id)
-  //       callback()
-  //     })
-
-    // }, function(err) {
-    //   if(err) console.error(err)
-
-    //   Connection.set(connections)
-
-    //   $scope.status = 'Computation complete'
-    //   $location.path('/schedule')
-    //   $scope.$apply()
-    //   // window.location.href = '/#/schedule'
-
-    // })
-
-  // }
+  })
 
 })
